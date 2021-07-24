82682995f9400d4218be393f2e6b8c3b522bcb21
Eric <>
2021-07-24 17:16:00 -0400

    Add testament input, mobile support, and left nav scroll

--- src/modules/common/home/home.component.ts
+++ src/modules/common/home/home.component.ts
@@ -18,7 +18,9 @@ interface Book {
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public books: Book[] = [];
+  public oldTestamentBooks: Book[] = [];
+  public newTestamentBooks: Book[] = [];
+  public displayOldTestament = true;
   public bookAbbreviations: string[] = [];
   public currentBook: Book = <Book>{ passage: 'Genesis', chapterCount: 50 };
   public bookTextHTML: string = '';
@@ -26,21 +28,27 @@ export class HomeComponent implements OnInit {
   public hoverActive = false;
   public currentChapter = 1;
 
+  public mobileUser = false;
+
   constructor(
     private _http: HttpClient,
     private _bookAbbreviatonPipe: BookAbbreviationPipe
   ) { }
 
   ngOnInit(): void {
+    this.mobileUser = typeof screen.orientation !== 'undefined';
     this._http.get('https://api.biblia.com/v1/bible/contents/ASV?key=' + environment.API_KEY).subscribe((booksObject: any) => {
-      this.books = booksObject.books.map((book: any) => {
+      const books = booksObject.books.map((book: any) => {
         return <Book>{
           passage: book.passage,
           chapterCount: book.chapters.length
         };
       });
-      this.bookAbbreviations = this.books.map((book: Book) => this._bookAbbreviatonPipe.transform(book.passage));
-      this.currentBook = <Book>{ passage: this.books[0].passage, chapterCount: this.books[0].chapterCount };
+      const matthewIndex = books.findIndex((book: Book) => book.passage.toLowerCase() === "matthew");
+      this.oldTestamentBooks = books.slice(0, matthewIndex);
+      this.newTestamentBooks = books.slice(matthewIndex, books.length);
+      this.bookAbbreviations = books.map((book: Book) => this._bookAbbreviatonPipe.transform(book.passage));
+      this.currentBook = <Book>{ passage: books[0].passage, chapterCount: books[0].chapterCount };
     });
 
     this._http.get('https://api.biblia.com/v1/bible/content/ASV.html?passage=Genesis1&style=fullyFormatted&key='
@@ -49,9 +57,15 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  setBook(newBook: Book) {
+  setBook(newBook: Book | string) {
     this.hoverActive = false;
-    this.currentBook = newBook;
+    if (typeof newBook === "string") {
+      const books = [...this.oldTestamentBooks, ...this.newTestamentBooks];
+      const bookIndex = books.findIndex((book: Book) => book.passage === newBook);
+      this.currentBook = <Book>{ passage: newBook, chapterCount: books[bookIndex].chapterCount };
+    } else {
+      this.currentBook = newBook;
+    }
     this.currentChapter = 1;
     this.activeBookAbbr = this._bookAbbreviatonPipe.transform(this.currentBook.passage);
     this._getBibleChapter(this.currentBook.passage, this.currentChapter);
@@ -60,6 +74,15 @@ export class HomeComponent implements OnInit {
   setChapter(chapterSelection: string) {
     this.currentChapter = parseInt(chapterSelection, 10);
     this._getBibleChapter(this.currentBook.passage, this.currentChapter);
+  }
+  
+  setTestament(testamentSelection: string) {
+    this.displayOldTestament = testamentSelection === "Old Testament";
+    if (this.displayOldTestament) {
+      this._getBibleChapter("Genesis", 1);
+    } else {
+      this._getBibleChapter("Matthew", 1);
+    }
   }
 
   mouseenter() {
